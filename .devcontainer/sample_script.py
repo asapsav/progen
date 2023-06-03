2cea8e3343363e073d630aa7cad8b69c77310352
Savelii Kholin <55986827+asapsav@users.noreply.github.com>
2023-06-03 23:27:11 +0000

    sample code

---
+++ .devcontainer/sample_script.py
@@ -0,0 +1,21 @@
+from jax import random
+from haiku import PRNGSequence
+from progen_transformer import ProGen
+
+model = ProGen(
+    num_tokens = 256,
+    dim = 512,
+    seq_len = 1024,
+    window_size = 256,       # local attention window size
+    depth = 12,              # depth
+    heads = 8,               # attention heads
+    dim_head = 64,           # dimension per head
+    ff_glu = True,           # use GLU in feedforward, from Noam's paper
+    global_mlp_depth = 2     # last N global gmlp layers
+)
+
+rng = PRNGSequence(42)
+seq = random.randint(next(rng), (1024,), 0, 256)
+
+params = model.init(next(rng), seq)
+logits = model.apply(params, next(rng), seq) # (1024, 256)
